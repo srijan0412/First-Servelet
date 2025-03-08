e1f617e6e3b93308a755ecf1cb1c75115c7586ee
srijan0412 <srijan.soni01@gmail.com>
2025-03-08 10:31:32 +0530

    [Srijan] Add . UC3

--- src/main/java/org/example/servlets/LoginServelet.java
+++ src/main/java/org/example/servlets/LoginServelet.java
@@ -14,6 +14,16 @@ public class LoginServlet extends HttpServlet {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
+        if (!username.matches("[A-Z][a-zA-Z]{2,}")) {  // Capital first letter, at least 3 characters
+            response.getWriter().println("Invalid Name! Name must start with a capital letter and have at least 3 characters.");
+            return;
+        }
+
+        if (!password.matches("^(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$")) {
+            response.getWriter().println("Invalid Password! Must have 8+ characters, 1 uppercase, 1 number, and exactly 1 special character.");
+            return;
+        }
+
         if ("admin".equals(username) && "admin123".equals(password)) {
             request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
         } else {
