0a8c795c0f2c97c161f39c3fb15103fd2a01039f
srijan0412 <srijan.soni01@gmail.com>
2025-03-08 10:27:12 +0530

    [Srijan] Add . Login servelet

---
+++ src/main/java/org/example/servlets/LoginServelet.java
@@ -0,0 +1,23 @@
+package org.example.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if ("admin".equals(username) && "admin123".equals(password)) {
+            request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
+        } else {
+            response.getWriter().println("Invalid Credentials!");
+        }
+    }
+}
